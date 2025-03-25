5d22bd72264ceb6f00d1ab8b0a978b28a431cd42
ionatecrz <193599444+ionatecrz@users.noreply.github.com>
2025-03-25 09:53:10 +0100

    Timers Header

--- Timers.h
+++ Timers.h
@@ -1,6 +1,10 @@
 #ifndef TIMERS_H
 #define TIMERS_H
 
+#include <stdint.h>
+
 void InicializarTimers(void);
+uint32_t getTiempoConfigurado(void);
+void setTiempoConfigurado(uint32_t tiempo);
 
 #endif
