913b6752a1cd18dff833ccc6770798c024fed7b0
Alejandra Botin <202101330@alu.comillas.edu>
2025-03-26 10:41:26 +0100

    Timer que cuenta ms, s, min y h (Con el Timer1)

---
+++ Timer.c
@@ -0,0 +1,45 @@
+#include <xc.h>
+#include "Pic32Ini.h"
+#include "Timer.h"
+
+void initTimer(void){
+
+    T1CON = 0;
+    TMR1 = 0;
+    PR1 = 4999; //Valor para 1 ms
+    IPC2bits.T1IP = 2;  
+    IPC2bits.T1IS = 0; 
+    IFS0bits.T1IF = 0; 
+    IEC0bits.T1IE = 1; 
+    T1CON = 0x8000;
+
+    INTCONbits.MVEC = 1; 
+    asm("ei"); 
+
+}
+
+//Rutina de Interrupcion del Timer 1
+void __attribute__((vector(4), interrupt(IPL2SOFT), nomips16)) InterrupcionTimer1(void){
+    IFS0bits.T1IF = 0;    
+    static int ms = 0;
+    static int s = 0;
+    static int min = 0;
+    static int h = 0;
+
+    ms++;
+    if (ms == 1000){
+        ms = 0;
+        s++;
+    }
+    if (s == 60){
+        s = 0;
+        min++;
+    }
+    if (min == 60){
+        min = 0;
+        h++;
+    }
+    if (h == 24){
+        h = 0;
+    }
+}
